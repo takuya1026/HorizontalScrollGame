7fdef41905e9508103400f1bb8c0ce66b83dd7ce
ochi.takuya <takuya.ochi4696@gmail.com>
2021-02-07 21:44:40 +0900

    add create stage and load file

---
+++ HorizontalScrollGame/Assets/MyGameAssets/Script/TestCode/TestCameraMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCameraMove : MonoBehaviour
+{
+    [SerializeField, Tooltip("カメラ")]
+    private Camera m_camera = null;
+
+    [SerializeField, Range(0.0f, 10.0f), Tooltip("カメラの移動量")]
+    private float m_speed = 0.0f;
+
+    void Update()
+    {
+        m_camera.gameObject.transform.Translate(new Vector3(m_speed, 0.0f, 0.0f));
+    }
+}
