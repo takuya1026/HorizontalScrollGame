d16bf77f909a83f9f78a44c99e7fc51cd4038977
takumyon <takumyon5145563@gmail.com>
2021-02-12 00:01:32 +0900

    スプレッドシートの内容をJSONに変換してファイル出力する

---
+++ HorizontalScrollGame/Assets/MyGameAssets/Script/Common/SpreadsheetUtility.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// スプレッドシート関連の処理で扱うやつ
+/// </summary>
+public static class SpreadsheetUtility
+{
+    /// <summary>
+    /// カルムのインデックスからアルファベットに変換
+    /// </summary>
+    public static string ColumnIndexToAlphabet(int index)
+    {
+        // FIXME:
+        // ひとまず、変換できるのはカラムがA~Zの場合のみ
+        // ABとかAABとかカラムがZを超えた場合は考慮しない。
+        // 流石にそこまでカラムが増えることはないだろう。
+
+        int a = (int)("A"[0]);
+        return ((char)(a + index)).ToString();
+    }
+}
