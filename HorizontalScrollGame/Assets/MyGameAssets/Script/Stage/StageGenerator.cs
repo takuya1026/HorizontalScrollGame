cdc07755bf31435b439e124247f104312c492b1d
ochi.takuya <takuya.ochi4696@gmail.com>
2021-02-12 02:15:07 +0900

    stage edit fix

--- HorizontalScrollGame/Assets/MyGameAssets/Script/Stage/StageGenerator.cs
+++ HorizontalScrollGame/Assets/MyGameAssets/Script/Stage/StageGenerator.cs
@@ -2,7 +2,7 @@
 //============================================================
 // @file StageGenerator
 // @brief ステージ生成
-// @autor 天才が作った
+// @autor ochi.takuya
 //============================================================
 
 using System.Collections;
@@ -14,8 +14,10 @@ using UnityEngine;
 /// </summary>
 public class StageGenerator : SingletonMonoBehaviour<StageGenerator>
 {
-    private GameObject m_baseObject = null;
     private GameObject m_parentObject = null;
+
+    private List<int> m_gropObjectIndex = new List<int>();
+    private Dictionary<string, GameObject> m_gropObject = new Dictionary<string, GameObject>();
 
     [SerializeField, Tooltip("デフォルトブロック")]
     private GameObject DEFAULT_BLOCK_OBJECT = null;
@@ -25,49 +27,102 @@ public class StageGenerator : SingletonMonoBehaviour<StageGenerator>
     /// </summary>
     public void Generate(StageInfo stageInfo)
     {
+        // NOTE: エディタ実行をしないため、毎回初期化
+        m_gropObjectIndex.Clear();
+        m_gropObject.Clear();
+
         if (! DEFAULT_BLOCK_OBJECT)
         {
             Debug.Log("ERROR: default block object is null. (StageController#placement)");
             return;
         }
 
-        m_baseObject = GameObject.Find(stageInfo.m_stageName);
-        if (m_baseObject != null)
+        m_parentObject = GameObject.Find(stageInfo.m_stageName);
+        if (m_parentObject != null)
         {
             Debug.Log("stage object 【 " + stageInfo.m_stageName  + " 】is Exists on the field. (StageGenerator#Generate)");
             return;
         }
 
-        GameObject newObject = null;
+        m_parentObject = new GameObject(stageInfo.m_stageName);
+        GameObject groupObject = null;
+        GameObject elementObject = null;
+        GameObject blockObject = null;
         BlockInfo blockInfo = null;
+
+        string oldGroupObjectName = "";
+        string oldElementObjectName = "";
+
+        for (int i = 0, length = stageInfo.m_resultBlockInfo.Length; i < length; i++)
+        {
+            if (m_parentObject == null)
+            {
+                continue;
+            }
+
+            if (stageInfo.m_resultBlockInfo[i].m_name.IndexOf("Group") >= 0
+             && stageInfo.m_resultBlockInfo[i].m_name != oldGroupObjectName)
+            {
+                if (m_gropObject.ContainsKey(stageInfo.m_resultBlockInfo[i].m_name))
+                {
+                    continue;
+                }
+
+                groupObject = new GameObject(stageInfo.m_resultBlockInfo[i].m_name);
+                groupObject.transform.parent = m_parentObject.transform;
+                oldGroupObjectName = stageInfo.m_resultBlockInfo[i].m_name;
+                m_gropObjectIndex.Add(i);
+                m_gropObject.Add(stageInfo.m_resultBlockInfo[i].m_name, groupObject);
+                continue;
+            }
+
+            if (stageInfo.m_resultBlockInfo[i].m_name.IndexOf("Element") >= 0
+             && stageInfo.m_resultBlockInfo[i].m_name != oldElementObjectName)
+            {
+                if (m_gropObject.ContainsKey(stageInfo.m_resultBlockInfo[i].m_name))
+                {
+                    continue;
+                }
+
+                elementObject = new GameObject(stageInfo.m_resultBlockInfo[i].m_name);
+                elementObject.transform.parent = groupObject.transform;
+                oldElementObjectName = stageInfo.m_resultBlockInfo[i].m_name;
+                m_gropObjectIndex.Add(i);
+                m_gropObject.Add(stageInfo.m_resultBlockInfo[i].m_name, elementObject);
+                continue;
+            }
+        }
+
         int imageIndex = 0;
         for (int i = 0, length = stageInfo.m_resultBlockInfo.Length; i < length; i++)
         {
-            if (m_baseObject == null)
+            if (m_gropObjectIndex.IndexOf(i) >= 0)
             {
-                m_baseObject = new GameObject(stageInfo.m_stageName);
-            }
-            if (m_parentObject == null)
-            {
-                m_parentObject = new GameObject("Stage");
-                m_parentObject.transform.parent = m_baseObject.transform;
+                continue;
             }
 
-            newObject = Instantiate(DEFAULT_BLOCK_OBJECT);
-            newObject.name = stageInfo.m_resultBlockInfo[i].m_name;
-            newObject.transform.position = stageInfo.m_resultBlockInfo[i].m_postion;
-            newObject.transform.rotation = stageInfo.m_resultBlockInfo[i].m_rotation;
-            newObject.transform.localScale = stageInfo.m_resultBlockInfo[i].m_scale;
+            if (!m_gropObject.ContainsKey(stageInfo.m_resultBlockInfo[i].m_parentName))
+            {
+                continue;
+            }
 
-            blockInfo = newObject.GetComponent<BlockInfo>();
+            blockObject = Instantiate(DEFAULT_BLOCK_OBJECT);
+            blockObject.name = stageInfo.m_resultBlockInfo[i].m_name;
+            blockObject.transform.position = stageInfo.m_resultBlockInfo[i].m_postion;
+            blockObject.transform.rotation = stageInfo.m_resultBlockInfo[i].m_rotation;
+            blockObject.transform.localScale = stageInfo.m_resultBlockInfo[i].m_scale;
+
+            blockInfo = blockObject.GetComponent<BlockInfo>();
             blockInfo.m_EnumBlockType = stageInfo.m_resultBlockInfo[i].m_enumBlockType;
             blockInfo.m_ItemId = stageInfo.m_resultBlockInfo[i].m_itemId;
             blockInfo.m_Quantity = stageInfo.m_resultBlockInfo[i].m_quantity;
 
             imageIndex = (int)stageInfo.m_resultBlockInfo[i].m_enumBlockType;
-            newObject.GetComponent<Renderer>().sharedMaterial = (Material)Resources.Load("Texture/Stage/Materials/MapTexture" + imageIndex.ToString("D2"));
+            blockObject.GetComponent<Renderer>().sharedMaterial = (Material)Resources.Load("Texture/Stage/Materials/MapTexture" + imageIndex.ToString("D2"));
 
-            newObject.transform.parent = m_parentObject.transform;
+            blockObject.transform.parent = m_gropObject[stageInfo.m_resultBlockInfo[i].m_parentName].transform;
         }
+
+
     }
 }
--- HorizontalScrollGame/Assets/MyGameAssets/Script/Stage/StageInfo.cs
+++ HorizontalScrollGame/Assets/MyGameAssets/Script/Stage/StageInfo.cs
@@ -18,6 +18,7 @@ public class StageInfo
     [System.Serializable]
     public struct ResultBlockInfo
     {
+        public string m_parentName;
         public string m_name;
         public Vector3 m_postion;
         public Quaternion m_rotation;
@@ -25,6 +26,7 @@ public class StageInfo
         public EnumBlockType m_enumBlockType;
         public int m_itemId;
         public int m_quantity;
+        public GameObject[] m_child;
     }
 
     public string m_stageName;
