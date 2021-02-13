5159e68e92883388de5f21aac3e46cb200362b07
takumyon <takumyon5145563@gmail.com>
2021-02-14 01:27:19 +0900

    マスターデータをゲーム開始時に読み込むシステムを作成

---
+++ HorizontalScrollGame/Assets/MyGameAssets/Script/MasterData/MasterDataManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// マスターデータ管理クラス
+/// </summary>
+public class MasterDataManager : SingletonMonoBehaviour<MasterDataManager>
+{
+    public EnemyMasterDataTable EnemyMasterDataTable { get; private set; } = new EnemyMasterDataTable();
+}
