46264989e1f009512f62dd1c961f7193aa367d33
ochi.takuya <takuya.ochi4696@gmail.com>
2021-02-17 00:29:04 +0900

    add gimmick script

---
+++ HorizontalScrollGame/Assets/MyGameAssets/Script/Gimmick/GimmickBase.cs
@@ -0,0 +1,24 @@
+﻿
+//============================================================
+// @file GimmickBase
+// @brief ギミックベース
+// @autor ochi takuya
+//============================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ギミックベース
+/// </summary>
+public class GimmickBase : MonoBehaviour
+{
+    // protected member
+    protected string m_name;
+    protected GimmickType m_gimmickType;
+    protected GameObject m_emptyObject;
+    protected GameObject m_protoObject;
+
+
+}
