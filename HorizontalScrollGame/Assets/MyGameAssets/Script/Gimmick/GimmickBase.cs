5bf96e46fd6714b133bbe95c6583850495257349
ochi.takuya <takuya.ochi4696@gmail.com>
2021-02-21 03:33:57 +0900

    add gimmick files

--- HorizontalScrollGame/Assets/MyGameAssets/Script/Gimmick/GimmickBase.cs
+++ HorizontalScrollGame/Assets/MyGameAssets/Script/Gimmick/GimmickBase.cs
@@ -12,13 +12,50 @@ using UnityEngine;
 /// <summary>
 /// ギミックベース
 /// </summary>
-public class GimmickBase : MonoBehaviour
+public class GimmickBase
 {
     // protected member
     protected string m_name;
     protected GimmickType m_gimmickType;
-    protected GameObject m_emptyObject;
-    protected GameObject m_protoObject;
+    protected GameObject m_object;
 
+    /// <summary>
+    /// 名前
+    /// </summary>
+    public string m_Name { get { return m_name; } protected set { m_name = value; } }
 
+    /// <summary>
+    /// ギミックタイプ
+    /// </summary>
+    public GimmickType m_GimmickType { get { return m_gimmickType; } protected set { m_gimmickType = value; } }
+
+    /// <summary>
+    /// オブジェクト
+    /// </summary>
+    public GameObject m_Object { get { return m_object; } protected set { m_object = value; } }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public GimmickBase() { /* 処置なし */ }
+
+    /// <summary>
+    /// デストラクタ
+    /// </summary>
+    ~GimmickBase() { /* 処置なし */ }
+
+    /// <summary>
+    /// 起動時処理
+    /// </summary>
+    public virtual void Awake() { /* 処置なし */ }
+
+    /// <summary>
+    /// 開始時処理
+    /// </summary>
+    public virtual void Start() { /* 処置なし */ }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    public virtual void Update() { /* 処置なし */ }
 }
---
+++ HorizontalScrollGame/Assets/MyGameAssets/Script/Gimmick/GimmickMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GimmickMove : GimmickBase
+{
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public GimmickMove(string name)
+    {
+        m_Name = name;
+    }
+
+    /// <summary>
+    /// デストラクタ
+    /// </summary>
+    ~GimmickMove()
+    {
+
+    }
+
+    /// <summary>
+    /// 開始時処理
+    /// </summary>
+    public override void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    public override void Update()
+    {
+        
+    }
+}
